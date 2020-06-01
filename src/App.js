073d3a145729c7ab84992231a356a140352488ac
Kamrannv <kamranamazov@yahoo.com>
2020-06-02 01:41:44 +0400

    stash

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React,{useEffect,useState} from 'react';
-import DEFAULT_LANG from './config/language'
-import LanguageService from './services/language.service'
-import './App.css';
+// import React,{useEffect,useState} from 'react';
+// import DEFAULT_LANG from './config/language'
+// import LanguageService from './services/language.service'
+// import './App.scss';
+// import Heading from './components/heading-chips/heading-chips.component';
   
-function App() {
-  const [lang,setLang]=useState({data:''});
-
-  useEffect(()=>{
-   let langData=LanguageService(DEFAULT_LANG);
-    setLang({data:langData})
-  });
+// function App() {
   
-  function languageEventHandler(e) {
-    console.log(e.target.value)
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <select>
-        <option onChange={languageEventHandler} defaultValue disabled>Select Language</option>
-         <option value='aze'>Azerbaijan</option>
-         <option  value='en'>English</option>
-        </select>
-        <p>
-          {lang.data.hello}
-        </p>
-      </header>
-    </div>
-  );
-}
+//   return (
+//    <React.Fragment>
+//     <Heading />
+   
+//    </React.Fragment>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ src/components/heading-chips/heading-chips.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+// import Chips from '../chips/chips.component'
+import './heading-chips.style.scss';
+
+function Heading(props){
+    return(
+        <div className="container">
+        <div className="row">
+                <div className="col-lg-5 col-md-12 col-sm-12">
+                    <div className="Slider">
+                        Slider
+                    </div>
+                </div>
+            <div className="col-lg-4 col-md-12 col-sm-12">
+                <h1>El ishleri</h1>
+        
+            
+            </div>
+
+
+
+
+
+            </div>
+        
+        
+        </div>
+       
+
+
+    )
+}
+
+export default Heading;
