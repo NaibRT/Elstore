882af510976b13963eb7ef9a48b30cb8ff354861
Kamrannv <kamranamazov@yahoo.com>
2020-06-02 23:22:33 +0400

    seller

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Heading from './components/heading-chips/heading-chips.component';
+import Seller from './components/seller/seller.component'
   
 function App() {
+  
   
   return (
    <React.Fragment>
     <Heading />
+    <Seller />
    
    </React.Fragment>
   );
