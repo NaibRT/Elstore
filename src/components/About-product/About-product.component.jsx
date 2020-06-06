f7e6ea827cffa449919a1e7a0aff787e01d961b2
nijataliyev <nijatsha@code.edu.az>
2020-06-06 21:44:57 +0400

    yeni dizayna uygun deyisdirildi

--- src/components/About-product/About-product.component.jsx
+++ src/components/About-product/About-product.component.jsx
@@ -7,6 +7,16 @@ import Db from '../../contexts/db.json';
 const data = Db.aboutProduct;
 
 function AboutProduct(){
+
+    function showButton(){
+       let aboutProductContent = document.getElementById('aboutContent');
+       let showMore = document.getElementById('showMore');
+       aboutProductContent.style.height = "100%";
+       aboutProductContent.style.overflow = "auto";
+       showMore.style.display = "none";
+
+    }
+
     return(
         <section className="about__product__section">
             {
@@ -16,9 +26,9 @@ function AboutProduct(){
                         <div className="about__product__title">
                             <h4>{s.title}</h4>
                         </div>
-                        <div className="about__product__content">
+                        <div id="aboutContent" className="about__product__content">
                             <p>
-                            Material
+                            Material:
                             cotton thread, wooden dowel, cotton yarn, loom.
                             This beautiful made to order bohemian style woven wall hanging is a 
                             lovely way to add some warmth and texture to your walls. Cozy textured neutrals, 
@@ -27,6 +37,7 @@ function AboutProduct(){
                             hand made using a range of carefully selected cotton and acrylic yarn, 
                             </p>
                         </div>
+                        <button id="showMore" onClick={showButton}>Davamını oxu</button>
                     </div>
                 ))
             }
