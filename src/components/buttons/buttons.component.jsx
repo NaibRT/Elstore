d15908320a4be9800591d019ef6a72e2fd9499fe
KhdjGahraman <khadija@pragmatech.az>
2020-06-06 21:41:52 +0400

    ready

--- src/components/buttons/buttons.component.jsx
+++ src/components/buttons/buttons.component.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import "./buttons.styles.scss";
-import Logo from './icon.svg';
+import Logo from "../buttons/Bag.svg";
 
 const Buttons = () => {
   return (
     <div className="btn-group">
-      
+      <div className="container">
         <div className="row">
-          <button className="btn-buy-now bg-primary border__radius--big border--medium display__block txt--light">
-           INDI AL
+          <button className="btn-add bg-primary border__radius--big border--medium display__block txt--light">
+            səbətə əlavə et
           </button>
 
-          <button className="btn-add bg-primary--light border__radius--big border--medium display__block txt--secondary">
-           SEBETE ELAVE ET
+          <button className="btn-buy-now border__radius--big border--medium display__block txt--secondary">
+            indi al
           </button>
 
           <div className="add-basket">
-            <img className="add-basket-icon display__inline-block" src={Logo} alt="icon" />
-            <p className="add-basket-p txt--secondary  display__inline-block">128 nefer <span className="add-basket-span txt--tertiary ">sebete elave edib</span></p>
+            <img
+              className="add-basket-icon display__inline-block"
+              src={Logo}
+              alt="icon"
+            />
+            <p className="add-basket-p txt--secondary  display__inline-block">
+              12 nəfər səbətə əlavə edib
+            </p>
           </div>
-
         </div>
+      </div>
     </div>
   );
 };
