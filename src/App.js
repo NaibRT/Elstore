b0de842fa260ad4be70975d9ac82ce78a7f3ebdb
Hasan109 <hasanei@code.edu.az>
2020-06-07 13:32:28 +0400

    ButtonsMap add

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import React from 'react';
 import './App.scss';
 import ProductInfo from './pages/product-info'
 import BasketPage from "./pages/basket"
+import ButtonsMap from "./components/buttonsMap/buttonsMap.component.jsx"
 function App() {
   return (
     <div className="App">
       <main>
-      {/* <ProductInfo/> */}
-       <BasketPage/>
+      <ProductInfo/>
+       {/* <BasketPage/> */}
+       {/* <ButtonsMap/> */}
       </main>
     </div>
   );
 }
 
 export default App;
-
 
 
