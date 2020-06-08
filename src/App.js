c8a96b244b00694acbdbb834326b8bb5357ed92a
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-06-08 05:41:33 -0700

    navbar ready

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import React from 'react';
 import './App.scss';
 import ProductInfo from './pages/product-info'
-import BasketPage from "./pages/basket"
+import BasketPage from "./pages/basket";
+import Index from './pages/index';
 function App() {
   return (
     <div className="App">
       <main>
        {/* <ProductInfo/> */}
-       <BasketPage/>
+       {/* <BasketPage/> */}
+       <Index />
       </main>
     </div>
   );
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,81 @@
+import React,{useState, useEffect} from 'react';
+import './navbar.component.scss';
+import { Link,  } from "react-router-dom";
+
+function  Navbar() {
+
+    const [toggle, setToggle] = useState({
+        active: false,
+      });
+
+    function toggleNav() {
+        const currentState = toggle.active;
+        setToggle({ active: !currentState });
+    }
+
+    return (
+        <div className='navbar'>
+            <div className='navbar_top'>
+                <Link className="navbar_top_link" to='#'>Kuryer olmaq istəyirsiniz?</Link>
+                <Link className="navbar_top_link navbar_top_link--end " to='#'>Mağaza aç</Link>
+            </div>
+            <div className='navbar_center'>
+                <div className='navbar_logo'>
+                    <img src={require('../../assets/images/heading/logo.svg')} />
+                </div>
+                <div className='navbar_search'>
+                <form className="search-input" action="/action_page.php">
+                    <input className='search-input-text' type="search" placeholder="Search.." name="search" />
+                    <button className='search-input-submit' type="submit"><img src={require('../../assets/images/icons/search.svg')} /></button>
+                </form>
+                </div>
+                <div className='navbar_buttons'>
+                    <Link className='navbar_buttons_link bag' to='/basket'> <img src={require('../../assets/images/heading/Bag.svg')} /></Link>
+                    <Link className='navbar_buttons_link log' to='/login'>daxİl ol</Link>
+                    <Link className='navbar_buttons_link log' to='/register'>hesab yarat</Link>
+                    <div>
+                    <div className={`${toggle.active ? 'change': ''} menu-container navbaroutside`}    onClick={toggleNav}>
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            
+            {/* <div className='searchDiv'>
+                <form className="search-input" action="/action_page.php">
+                    <input className='search-input-text' type="search" placeholder="Search.." name="search" />
+                    <button className='search-input-submit' type="submit"><img src={require('../../assets/images/icons/search.svg')} /></button>
+                </form>
+            </div> */}
+
+            <div className={`${toggle.active ? 'opennav': ''} responsive_nav`}>
+                    <div className='responsive_nav_top'>
+                        <Link className='responsive_nav_top_lang' to='/eng'>Eng</Link>
+                        <Link className='responsive_nav_top_lang' to='/ru'>Ru</Link>
+                    </div>
+                    <div className='responsive_nav_login'>
+                        <Link className='responsive_nav_login_log' to='/register'>Register</Link>
+                        <Link className='responsive_nav_login_log' to='/login'>Login</Link>
+                    </div>
+                    <div className='responsive_nav_bottom'>
+                        <Link className='responsive_nav_bottom_item' to="/cabinet">Cabinet <img src={require('../../assets/images/icons/arrowDown.png')} /> </Link>
+                        <Link className='responsive_nav_bottom_item' to="/categories">Kategories <img src={require('../../assets/images/icons/arrowDown.png')} /></Link>
+                    </div>
+                </div>
+
+            <div className='navbar_bottom'>
+                <Link to='/homeandoffice' className='navbar_bottom_link'>Ev və ofis aksesuarları</Link>
+                <Link to='/clothesandbags' className='navbar_bottom_link'>Geyim və çantalar</Link>
+                <Link to='/bujiteriya' className='navbar_bottom_link'>Bijuteriya və aksesuarlar</Link>
+                <Link to='/special' className='navbar_bottom_link'>Özəlləşdirilən</Link>
+                <Link to='/art' className='navbar_bottom_link'>İncəsənət nümunələri</Link>
+                <Link to='/handcraft' className='navbar_bottom_link'>Əl işləri</Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
