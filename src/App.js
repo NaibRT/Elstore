6d703fb9718d3db0cd8da2ef04f646e926de1621
Hasan109 <hasanei@code.edu.az>
2020-06-09 00:15:49 +0400

    Category done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.scss';
 import ProductInfo from './pages/product-info'
 import BasketPage from "./pages/basket"
 import ButtonsMap from "./components/buttonsMap/buttonsMap.component.jsx"
+import Category from "./components/Category/Category.component.jsx"
 function App() {
   return (
     <div className="App">
       <main>
-      <ProductInfo/>
+      {/* <ProductInfo/> */}
        {/* <BasketPage/> */}
        {/* <ButtonsMap/> */}
+       <Category/>
       </main>
     </div>
   );
