eae8b31cdd5077e9392d60eabd7042b04ee9b48c
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-06-09 04:07:41 -0700

    mehsulCard added

--- src/components/mehsulCard/mehsul_card.component.jsx
+++ src/components/mehsulCard/mehsul_card.component.jsx
@@ -43,14 +43,14 @@ function MehsulCard() {
                     card.map(({card_head,card_back,card_store_img,card_store})=>{
                         return(
                             <div className='col-lg-6 col-md-12'>
-                        <div style={{backgroundImage: "url("+card_back +")"}} class="card">
-                            <div class="card-overlay">
-                                <p class="card-head">{card_head}</p>
-                                <div class="card-info">
-                                    <img class="card-info-name" src={card_store_img} />
-                                    <p class='card-info-text'>{card_store}</p>
+                        <div style={{backgroundImage: "url("+card_back +")"}} class="e-card">
+                            <div class="e-card-overlay">
+                                <p class="e-card-head">{card_head}</p>
+                                <div class="e-card-info">
+                                    <img class="e-card-info-name" src={card_store_img} />
+                                    <p class='e-card-info-text'>{card_store}</p>
                                     <div>
-                                        <Link to='/product' class='card-go'>Məhsullara bax <img src={require('../../assets/images/icons/right.svg')} /></Link>
+                                        <Link to='/product' class='e-card-go'>Məhsullara bax <img src={require('../../assets/images/icons/right.svg')} /></Link>
                                     </div>
                                 </div>
                             </div>
