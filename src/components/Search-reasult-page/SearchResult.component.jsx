4b55c09cf12a2b09f2e713d99ca14663ed957a9c
nijataliyev <nijatsha@code.edu.az>
2020-06-11 19:23:30 +0400

    index_and_search_page_edit

--- src/components/Search-reasult-page/SearchResult.component.jsx
+++ src/components/Search-reasult-page/SearchResult.component.jsx
@@ -5,7 +5,7 @@ import './SearchResult.component.scss'
 import ButtonRating from '../button-rating/buttonRating.component';
 import HeartImage from '../heart-image/heartImage.component';
 import {Link} from 'react-router-dom'
-export class SearchResult extends Component {
+
 function SearchResult() {
 const products=useContext(searchContext);
 
@@ -37,7 +37,11 @@ const products=useContext(searchContext);
 
                         })
                     }
-                    <h5>{y.product_name}</h5>
+                    <h5>
+                        <a>
+                            {y.product_name}
+                        </a>
+                    </h5>
                     <p>{y.price} AZN</p>
                     <ButtonRating/>
                 </div>
