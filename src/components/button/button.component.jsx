9646db684cce3ba9ab86d244c7f6803c46ca0324
NaibRT <naibrt@code.edu.az>
2020-06-29 19:09:15 +0400

    modified button component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import './button.component.scss';
 
 
-const Button = ({children,onClick,type,name,data,className}) => {
+const Button = ({children,onClick,type,name,data,className,btng}) => {
   return (
-    <div className="btn-group">
+    <div className={`btn-group ${btng} `}>
           <button data={data} onClick={onClick} type={type} className={`btn-add btn border__radius--big border--medium display__block  ${className}`}>
           {name}
           {children}
