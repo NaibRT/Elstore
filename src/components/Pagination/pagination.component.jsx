a7953fdfe8618cc73702a5308bb7cf4c1a97bded
NaibRT <naibrt@code.edu.az>
2020-08-19 17:14:12 +0400

    modified conflict pages

--- src/components/Pagination/pagination.component.jsx
+++ src/components/Pagination/pagination.component.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import "./pagination.scss"
-function Pagination({ meta, paginationHandling, prop }) {
-  const pageItems = [];
 
 function Pagination({meta,paginationHandling,prop}){
    
-const pageItems=[]
-
-
+ const pageItems=[]
 for(let i=0;i<meta.last_page;i++){
     if(meta.to!=null){
     pageItems.push(i)
-}
+    }
 }
     console.log('pagination prop',prop)
     return (
